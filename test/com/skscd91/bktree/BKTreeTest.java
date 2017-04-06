fa34a9f369c24b7afa9b4b3183645c1a95a80228
sk-scd91 <scdeneen@gmail.com>
2017-04-06 15:49:53 -0400

    Added JUnit test cases.

---
+++ test/com/skscd91/bktree/BKTreeTest.java
@@ -0,0 +1,106 @@
+package com.skscd91.bktree;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * Created by Sean Deneen on 4/5/17.
+ */
+class BKTreeTest {
+
+    private static final String[] testStrings = new String[]{"some", "soft", "same", "mole", "soda", "salmon"};
+    private static final DistanceFunction<CharSequence> distFunc = DistanceFunctions.levenshteinDistance();
+
+    @Test
+    void add() {
+        BKTree<CharSequence> tree = new BKTree<>(distFunc);
+        assertTrue(tree.add("some"), "first add");
+        assertFalse(tree.add("some"), "add equivalent");
+        assertTrue(tree.add("somb"), "add new object");
+        assertFalse(tree.add("somb"), "add equivalent again");
+        assertThrows(NullPointerException.class, ()->tree.add(null));
+    }
+
+    @Test
+    void search() {
+        BKTree<CharSequence> tree = new BKTree<>(distFunc);
+        for (String s : testStrings)
+            tree.add(s);
+        List<BKTree.SearchResult<CharSequence>> results = tree.search("sort", 2);
+        assertEquals(3, results.size(), "result size");
+        assertEquals(1, results.get(0).getDistance(), "sorted by distance");
+        assertEquals("soft", results.get(0).getItem());
+
+        assertTrue(results.get(1).getDistance() == results.get(2).getDistance(),
+                "last 2 are same distance");
+        assertEquals(2, results.get(1).getDistance(), "last 2 have distance of 2");
+        if ("some".equals(results.get(1).getItem())) {
+            assertEquals("soda", results.get(2).getItem());
+        } else {
+            assertEquals("soda", results.get(1).getItem());
+            assertEquals("some", results.get(2).getItem());
+        }
+        assertTrue(tree.search(null, 2).isEmpty(), "no search when t is null");
+    }
+
+    @Test
+    void contains() {
+        BKTree<CharSequence> tree = new BKTree<>(distFunc);
+        assertFalse(tree.contains("some"), "false if empty");
+        for (String s : testStrings)
+            tree.add(s);
+        for (String s : testStrings) {
+            assertTrue(tree.contains(s), s + " is in tree");
+            assertTrue(tree.contains(new StringBuffer(s)), s + " equivalent CharSequence is in tree");
+        }
+        assertFalse(tree.contains("sort"), "sort not in tree");
+        assertFalse(tree.contains(null), "null not in tree");
+        assertFalse(tree.contains(new Object()), "non CharSequence not in tree");
+    }
+
+    @Test
+    void remove() {
+        BKTree<CharSequence> tree = new BKTree<>(distFunc);
+        assertFalse(tree.remove("some"), "false if empty");
+        for (String s : testStrings)
+            tree.add(s);
+        assertFalse(tree.remove("sort"), "sort not in tree");
+        assertFalse(tree.remove(null), "null not in tree");
+        assertFalse(tree.remove(new Object()), "non CharSequence not in tree");
+
+        assertTrue(tree.remove("same"), "node without children was removed");
+        assertTrue(tree.remove("soft"), "node with children removed");
+        assertEquals(2, tree.search("sort", 2).size(), "confirm node was removed");
+        assertTrue(tree.remove("soft"), "node with children removed");
+    }
+
+    @Test
+    void iterator() {
+        String[] bfsTestStrings = new String[] {"some", "same", "soft", "salmon", "soda", "mole"};
+        BKTree<CharSequence> tree = new BKTree<>(distFunc);
+        assertFalse(tree.iterator().hasNext(), "false if empty");
+        for (String s : testStrings)
+            tree.add(s);
+        int i = 0;
+        for (CharSequence s : tree) {
+            assertTrue(i < bfsTestStrings.length);
+            assertEquals(bfsTestStrings[i], s, bfsTestStrings[i] + " is equal");
+        }
+    }
+
+    @Test
+    void size() {
+        BKTree<CharSequence> tree = new BKTree<>(distFunc);
+        for (String s : testStrings)
+            tree.add(s);
+        assertEquals(testStrings.length, tree.size(), "initial size");
+        tree.add("sort");
+        assertEquals(testStrings.length + 1, tree.size(), "one added");
+        tree.remove("sort");
+        assertEquals(testStrings.length, tree.size(), "one removed");
+    }
+
+}
---
+++ test/com/skscd91/bktree/DistanceFunctionsTest.java
@@ -0,0 +1,74 @@
+package com.skscd91.bktree;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * Created by Sean Deneen on 4/5/17.
+ */
+class DistanceFunctionsTest {
+    @Test
+    void hammingDistanceEqualsCaseSensitive() {
+        DistanceFunction<CharSequence> hammingDistance = DistanceFunctions.hammingDistance();
+        assertEquals(0, hammingDistance.distance("test", "test"));
+        assertEquals(4, hammingDistance.distance("test", "TEST"));
+    }
+
+    @Test
+    void hammingDistanceEqualsCaseInsensitive() {
+        DistanceFunction<CharSequence> hammingDistance = DistanceFunctions.hammingDistance(false);
+        assertEquals(0, hammingDistance.distance("test", "test"), "same case");
+        assertEquals(0, hammingDistance.distance("test", "TEST"), "different case");
+    }
+
+    @Test
+    void hammingDistance() {
+        DistanceFunction<CharSequence> hammingDistance = DistanceFunctions.hammingDistance();
+        assertEquals(1, hammingDistance.distance("same","some"), "'same' and 'some'");
+        assertEquals(1, hammingDistance.distance("same", "sam"), "'same' and 'sam");
+        assertEquals(1, hammingDistance.distance("sam", "same"), "'sam' and 'same");
+        assertEquals(4, hammingDistance.distance("same", "abcd"), "'same' and 'abcd'");
+        assertEquals(9, hammingDistance.distance("same", "different"), "'same' and 'different'");
+    }
+
+    @Test
+    void levenshteinDistanceEqualsCaseSensitive() {
+        DistanceFunction<CharSequence> levenshteinDistance = DistanceFunctions.levenshteinDistance();
+        assertEquals(0, levenshteinDistance.distance("test", "test"));
+        assertEquals(4, levenshteinDistance.distance("test", "TEST"));
+    }
+
+    @Test
+    void levenshteinDistanceEqualsCaseInensitive() {
+        DistanceFunction<CharSequence> levenshteinDistance = DistanceFunctions.levenshteinDistance(false);
+        assertEquals(0, levenshteinDistance.distance("test", "test"), "same case");
+        assertEquals(0, levenshteinDistance.distance("test", "TEST"), "different case");
+    }
+
+    @Test
+    void levenshteinDistanceEmpty() {
+        DistanceFunction<CharSequence> levenshteinDistance = DistanceFunctions.levenshteinDistance();
+        assertEquals(4, levenshteinDistance.distance("", "test"), "left empty");
+        assertEquals(4, levenshteinDistance.distance("test", ""), "right empty");
+    }
+
+    @Test
+    void levenshteinDistance() {
+        DistanceFunction<CharSequence> levenshteinDistance = DistanceFunctions.levenshteinDistance();
+        assertEquals(1, levenshteinDistance.distance("same", "some"),"'same' and 'some'");
+        assertEquals(1, levenshteinDistance.distance("same", "sam"), "'same' and 'sam");
+        assertEquals(1, levenshteinDistance.distance("sam", "same"), "'sam' and 'same");
+        assertEquals(1, levenshteinDistance.distance("same", "ame"), "'same' and 'ame");
+        assertEquals(1, levenshteinDistance.distance("ame", "same"), "'ame' and 'same");
+
+        assertEquals(2, levenshteinDistance.distance("some", "soft"),"'some' and 'soft'");
+        assertEquals(2, levenshteinDistance.distance("some", "soda"),"'some' and 'soda'");
+        assertEquals(2, levenshteinDistance.distance("some", "mole"),"'some' and 'mole'");
+        assertEquals(2, levenshteinDistance.distance("soft", "soda"),"'soft' and 'soda'");
+        assertEquals(3, levenshteinDistance.distance("soft", "mole"),"'soft' and 'mole'");
+
+        assertEquals(4, levenshteinDistance.distance("some", "salmon"),"'same' and 'salmon'");
+    }
+
+}
